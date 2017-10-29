3c4fb2f36d6b2e8da55103162c16b229d5e9ea1b
Agustin Feuerhake <agustinfeuerhake@gmail.com>
2017-10-29 20:15:58 -0300

    feat(luck): make it a matter of luck

--- lib/lita/handlers/lunch_reminder.rb
+++ lib/lita/handlers/lunch_reminder.rb
@@ -52,26 +52,24 @@ module Lita
       end
       route(/^sí$|^hoy almuerzo aquí?i?$|^si$/i,
         command: true, help: help_msg(:confirm_yes)) do |response|
-        success = add_to_current_lunchers(response.user.mention_name)
+        add_to_current_lunchers(response.user.mention_name)
+        add_to_winning_lunchers(response.user.mention_name) if already_assigned?
         lunchers = current_lunchers_list.length
-        if success
-          case lunchers
-          when 1
-            response.reply(t(:current_lunchers_one))
-          when 2..10
-            response.reply(t(:current_lunchers_some, subject: lunchers))
-          end
+        case lunchers
+        when 1
+          response.reply(t(:current_lunchers_one))
         else
-          response.reply(t(:current_lunchers_too_many))
+          response.reply(t(:current_lunchers_some, subject: lunchers))
         end
       end
       route(/no almuerzo/i, command: true, help: help_msg(:confirm_no)) do |response|
         remove_from_current_lunchers response.user.mention_name
         response.reply(t(:thanks_for_answering))
+        remove_from_winning_lunchers response.user.mention_name
       end
 
       route(/tengo un invitado/i, command: true) do |response|
-        if add_to_current_lunchers("invitado_de_#{response.user.mention_name}")
+        if add_to_winning_lunchers("invitado_de_#{response.user.mention_name}")
           response.reply(t(:friend_added, subject: response.user.mention_name))
         else
           response.reply("tu amigo no cabe wn")
@@ -83,24 +81,34 @@ module Lita
       end
 
       route(/quié?e?nes almuerzan hoy/i, help: help_msg(:show_today_lunchers)) do |response|
-        case current_lunchers_list.length
+        unless already_assigned?
+          response.reply("Aun no lo se pero van #{current_lunchers_list.count} interesados.")
+          return
+        end
+        case winning_lunchers_list.length
         when 0
           response.reply(t(:no_one_lunches))
         when 1
-          response.reply(t(:only_one_lunches, subject: current_lunchers_list[0]))
+          response.reply(t(:only_one_lunches, subject: winning_lunchers_list[0]))
         when 2
           response.reply(t(:dinner_for_two,
-            subject1: current_lunchers_list[0],
-            subject2: current_lunchers_list[1]))
+            subject1: winning_lunchers_list[0],
+            subject2: winning_lunchers_list[1]))
         else
           response.reply(t(:current_lunchers_list,
-            subject1: current_lunchers_list.length,
-            subject2: current_lunchers_list.join(', ')))
+            subject1: winning_lunchers_list.length,
+            subject2: winning_lunchers_list.join(', ')))
         end
       end
 
       route(/quié?e?nes no almuerzan hoy/i, help: help_msg(:show_today_not_lunchers)) do |response|
         response.reply(t(:wont_lunch, subject: wont_lunch.join(', ')))
+      end
+
+      route(/resetredis/i) do |response|
+        redis.del('current_lunchers')
+        redis.del('lunchers')
+        response.reply("ok")
       end
 
       route(/quié?e?nes está?a?n considerados para (el|los) almuerzos?/i,
@@ -109,9 +117,12 @@ module Lita
       end
 
       route(/cédele mi puesto a ([^\s]+)/i, command: true) do |response|
-        remove_from_current_lunchers(response.user.mention_name)
+        unless remove_from_winning_lunchers(response.user.mention_name)
+          response.reply("no puedes ceder algo que no tienes, amiguito")
+          return
+        end
         enters = response.matches[0][0]
-        add_to_current_lunchers(enters)
+        add_to_winning_lunchers(enters)
         response.reply("tú te lo pierdes, comerá #{enters} por ti")
       end
 
@@ -121,7 +132,7 @@ module Lita
       end
 
       def refresh
-        reset_current_lunchers
+        reset_lunchers
         lunchers_list.each do |luncher|
           user = Lita::User.find_by_mention_name(luncher)
           message = t(:question, subject: luncher)
@@ -140,6 +151,15 @@ module Lita
         redis.sadd("lunchers", mention_name)
       end
 
+      def add_to_winning_lunchers(mention_name)
+        if winning_lunchers_list.count <= ENV['MAX_LUNCHERS'].to_i
+          redis.sadd("winning_lunchers", mention_name)
+          true
+        else
+          false
+        end
+      end
+
       def remove_from_lunchers(mention_name)
         redis.srem("lunchers", mention_name)
       end
@@ -149,42 +169,67 @@ module Lita
       end
 
       def add_to_current_lunchers(mention_name)
-        if current_lunchers_list.length < ENV['MAX_LUNCHERS'].to_i
-          redis.sadd("current_lunchers", mention_name)
-          true
-        else
-          false
-        end
+        redis.sadd("current_lunchers", mention_name)
       end
 
       def remove_from_current_lunchers(mention_name)
         redis.srem("current_lunchers", mention_name)
       end
 
-      def persist_current_lunchers
+      def remove_from_winning_lunchers(mention_name)
+        redis.srem("winning_lunchers", mention_name)
+      end
+
+      def persist_winning_lunchers
         sw = Lita::Services::SpreadsheetWriter.new
-        sw.write_new_row([Time.now.strftime("%Y-%m-%d")].concat(current_lunchers_list))
+        sw.write_new_row([Time.now.strftime("%Y-%m-%d")].concat(winning_lunchers_list))
       end
 
       def current_lunchers_list
         redis.smembers("current_lunchers") || []
       end
 
+      def winning_lunchers_list
+        redis.smembers("winning_lunchers") || []
+      end
+
       def wont_lunch
         redis.sdiff("lunchers", "current_lunchers")
       end
 
-      def reset_current_lunchers
+      def reset_lunchers
         redis.del("current_lunchers")
+        redis.del("winning_lunchers")
+        redis.del("already_assigned")
+      end
+
+      def pick_winners
+        current_lunchers_list.sample(ENV['MAX_LUNCHERS'].to_i).each do |winner|
+          add_to_winning_lunchers winner
+        end
+      end
+
+      def already_assigned?
+        redis.get("already_assigned") ? true : false
+      end
+
+      def announce_winners
+        notify(winning_lunchers_list, "Yeah baby, almuerzas con nosotros!")
+        notify(current_lunchers_list - winning_lunchers_list, t(:current_lunchers_too_many))
       end
 
       def create_schedule
         scheduler = Rufus::Scheduler.new
         scheduler.cron(ENV['ASK_CRON']) do
           refresh
+          scheduler.in(ENV['WAIT_RESPONSES_SECONDS'].to_i) do
+            pick_winners
+            redis.set("already_assigned", true)
+            announce_winners
+          end
         end
         scheduler.cron(ENV['PERSIST_CRON']) do
-          persist_current_lunchers
+          persist_winning_lunchers
         end
       end
 
