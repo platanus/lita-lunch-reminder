c6fd4193321b749bb4d847b993ba6d3e36af402f
Andres Matte <matte.andres@gmail.com>
2018-09-28 10:59:53 -0300

    feat(api): add endpoint to transfer lunch

--- spec/lita/handlers/api/lunch_spec.rb
+++ spec/lita/handlers/api/lunch_spec.rb
@@ -101,4 +101,79 @@ describe Lita::Handlers::Api::Lunch, lita_handler: true do
       end
     end
   end
+
+  describe '#transfer' do
+    context 'not authorized' do
+      it 'responds with not autorized' do
+        response = JSON.parse(http.post('current_lunchers').body)
+        expect(response['status']).to eq(401)
+        expect(response['message']).to eq('Not authorized')
+      end
+    end
+
+    context 'authorized' do
+      context 'receiver doesn\'t exist' do
+        before do
+          allow_any_instance_of(Rack::Request).to receive(:params).and_return(user_id: pedro.id)
+          @response = JSON.parse(http.post do |req|
+            req.url 'lunches/transfer'
+            req.body = "{\"receiver\": \"batman\"}"
+          end.body)
+        end
+
+        it { expect(@response['status']).to eq(404) }
+        it { expect(@response['message']).to eq('Error in parameters') }
+
+        it 'should not remove sender from winning lunchers' do
+          winning_lunchers = assigner.winning_lunchers_list
+          expect(winning_lunchers).to include(pedro.mention_name)
+        end
+      end
+
+      context 'sender did not win lunch' do
+        before do
+          allow_any_instance_of(Rack::Request).to receive(:params).and_return(user_id: juan.id)
+          @response = JSON.parse(http.post do |req|
+            req.url 'lunches/transfer'
+            req.body = "{\"receiver\": \"#{pedro.mention_name}\"}"
+          end.body)
+        end
+
+        it { expect(@response['status']).to eq(404) }
+        it { expect(@response['message']).to eq('User not in winning lunchers') }
+
+        it 'should not remove sender from winning lunchers' do
+          winning_lunchers = assigner.winning_lunchers_list
+          expect(winning_lunchers).to include(pedro.mention_name)
+        end
+
+        it 'should not add receiver to winning lunchers' do
+          winning_lunchers = assigner.winning_lunchers_list
+          expect(winning_lunchers).not_to include(juan.mention_name)
+        end
+      end
+
+      context 'sender won lunch' do
+        before do
+          allow_any_instance_of(Rack::Request).to receive(:params).and_return(user_id: pedro.id)
+          @response = JSON.parse(http.post do |req|
+            req.url 'lunches/transfer'
+            req.body = "{\"receiver\": \"#{juan.mention_name}\"}"
+          end.body)
+        end
+
+        it { expect(@response['success']).to be(true) }
+
+        it 'should remove sender from winning lunchers' do
+          winning_lunchers = assigner.winning_lunchers_list
+          expect(winning_lunchers).not_to include(pedro.mention_name)
+        end
+
+        it 'should add receiver to winning lunchers' do
+          winning_lunchers = assigner.winning_lunchers_list
+          expect(winning_lunchers).to include(juan.mention_name)
+        end
+      end
+    end
+  end
 end
