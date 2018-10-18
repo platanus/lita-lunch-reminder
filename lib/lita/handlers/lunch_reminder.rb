5d681b248f98e1f0e61e3f1ea63938716679b604
fdominguezc <fdominguezc@uc.cl>
2018-10-18 18:17:36 -0300

    fix(market): add missing parameters

--- lib/lita/handlers/lunch_reminder.rb
+++ lib/lita/handlers/lunch_reminder.rb
@@ -9,7 +9,7 @@ module Lita
         super
         @karmanager = Lita::Services::Karmanager.new(redis)
         @assigner = Lita::Services::LunchAssigner.new(redis, @karmanager)
-        @market = Lita::Services::MarketManager.new(redis)
+        @market = Lita::Services::MarketManager.new(redis, @assigner, @karmanager)
       end
 
       def self.help_msg(route)
