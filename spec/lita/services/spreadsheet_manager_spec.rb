dae9133b0f7bf55e59cfb9d491b998c395171787
fdominguezc <fdominguezc@uc.cl>
2018-12-06 18:11:01 -0300

    test(Spreadsheet Manager): refactor tests

--- spec/lita/services/spreadsheet_manager_spec.rb
+++ spec/lita/services/spreadsheet_manager_spec.rb
@@ -39,55 +39,55 @@ context 'on a new empty worksheet' do
     )
   end
 
-  before do
-    allow(ENV).to receive(:fetch)
-      .with('GOOGLE_SP_KEY')
-      .and_return('ENV_GOOGLE_SP_KEY')
-    # credentials_io:
-    allow(ENV).to receive(:fetch)
-      .with('GOOGLE_SP_CRED_TYPE')
-      .and_return('TYPE')
-    allow(ENV).to receive(:fetch)
-      .with('GOOGLE_SP_CRED_PROJECT_ID')
-      .and_return('PROJECT_ID')
-    allow(ENV).to receive(:fetch)
-      .with('GOOGLE_SP_CRED_PRIVATE_KEY_ID')
-      .and_return('PRIVATE_KEY_ID')
-    allow(ENV).to receive(:fetch)
-      .with('GOOGLE_SP_CRED_PRIVATE_KEY')
-      .and_return('UFJJVkFURV9LRVk=') # Base64.strict_decode64(UFJJVkFURV9LRVk=) == PRIVATE_KEY
-    allow(ENV).to receive(:fetch)
-      .with('GOOGLE_SP_CRED_CLIENT_EMAIL')
-      .and_return('CLIENT_EMAIL')
-    allow(ENV).to receive(:fetch)
-      .with('GOOGLE_SP_CRED_CLIENT_ID')
-      .and_return('CLIENT_ID')
-    allow(ENV).to receive(:fetch)
-      .with('GOOGLE_SP_CRED_AUTH_URI')
-      .and_return('AUTH_URI')
-    allow(ENV).to receive(:fetch)
-      .with('GOOGLE_SP_CRED_TOKEN_URI')
-      .and_return('TOKEN_URI')
-    allow(ENV).to receive(:fetch)
-      .with('GOOGLE_SP_CRED_AUTH_PROVIDER_X509_CERT_URL')
-      .and_return('AUTH_PROVIDER_X509_CERT_URL')
-    allow(ENV).to receive(:fetch)
-      .with('GOOGLE_SP_CRED_CLIENT_X509_CERT_URL')
-      .and_return('CLIENT_X509_CERT_URL')
-
-    expect(GoogleDrive::Session).to receive(:from_service_account_key)
-      .with(expected_session_credentials)
-      .and_return(google_session_mock)
-    google_session_mock = GoogleDrive::Session
-                          .from_service_account_key(expected_session_credentials)
-
-    expect(google_session_mock).to receive(:spreadsheet_by_key)
-      .with(ENV.fetch('GOOGLE_SP_KEY'))
-      .and_return(google_spreasheet_mock)
-    google_session_mock.spreadsheet_by_key('ENV_GOOGLE_SP_KEY')
-  end
-
   describe '#write_new_row' do
+    before do
+      allow(ENV).to receive(:fetch)
+        .with('GOOGLE_SP_KEY')
+        .and_return('ENV_GOOGLE_SP_KEY')
+      # credentials_io:
+      allow(ENV).to receive(:fetch)
+        .with('GOOGLE_SP_CRED_TYPE')
+        .and_return('TYPE')
+      allow(ENV).to receive(:fetch)
+        .with('GOOGLE_SP_CRED_PROJECT_ID')
+        .and_return('PROJECT_ID')
+      allow(ENV).to receive(:fetch)
+        .with('GOOGLE_SP_CRED_PRIVATE_KEY_ID')
+        .and_return('PRIVATE_KEY_ID')
+      allow(ENV).to receive(:fetch)
+        .with('GOOGLE_SP_CRED_PRIVATE_KEY')
+        .and_return('UFJJVkFURV9LRVk=') # Base64.strict_decode64(UFJJVkFURV9LRVk=) == PRIVATE_KEY
+      allow(ENV).to receive(:fetch)
+        .with('GOOGLE_SP_CRED_CLIENT_EMAIL')
+        .and_return('CLIENT_EMAIL')
+      allow(ENV).to receive(:fetch)
+        .with('GOOGLE_SP_CRED_CLIENT_ID')
+        .and_return('CLIENT_ID')
+      allow(ENV).to receive(:fetch)
+        .with('GOOGLE_SP_CRED_AUTH_URI')
+        .and_return('AUTH_URI')
+      allow(ENV).to receive(:fetch)
+        .with('GOOGLE_SP_CRED_TOKEN_URI')
+        .and_return('TOKEN_URI')
+      allow(ENV).to receive(:fetch)
+        .with('GOOGLE_SP_CRED_AUTH_PROVIDER_X509_CERT_URL')
+        .and_return('AUTH_PROVIDER_X509_CERT_URL')
+      allow(ENV).to receive(:fetch)
+        .with('GOOGLE_SP_CRED_CLIENT_X509_CERT_URL')
+        .and_return('CLIENT_X509_CERT_URL')
+
+      expect(GoogleDrive::Session).to receive(:from_service_account_key)
+        .with(expected_session_credentials)
+        .and_return(google_session_mock)
+      google_session_mock = GoogleDrive::Session
+                            .from_service_account_key(expected_session_credentials)
+
+      expect(google_session_mock).to receive(:spreadsheet_by_key)
+        .with(ENV.fetch('GOOGLE_SP_KEY'))
+        .and_return(google_spreasheet_mock)
+      google_session_mock.spreadsheet_by_key('ENV_GOOGLE_SP_KEY')
+    end
+
     context 'when given an array with values' do
       it "should write the array in the last row, each value in it's own column" do
         array = [*0..5]
@@ -109,13 +109,6 @@ context 'on a new empty worksheet' do
         write_flag = spreadsheetManager.write_new_row([])
         expect(write_flag).to eq(false)
       end
-    end
-  end
-
-  describe '#read' do
-    it 'returns a worksheet instance' do
-      allow(spreadsheetManager).to receive(:read).with('WS_SHEET').and_return(worksheet_mock)
-      expect(spreadsheetManager.read('WS_SHEET')).to eq(worksheet_mock)
     end
   end
 end
